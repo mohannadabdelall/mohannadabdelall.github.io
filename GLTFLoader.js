e0eb2b55231aacc5d43271a9b5b52dde6acc2ed7
ahmadkammonah <57930224+ahmadkammonah@users.noreply.github.com>
2020-09-20 11:13:55 -0300

    Delete GLTFLoader.js

--- GLTFLoader.js
+++
@@ -1,7 +0,0 @@
-let loader = new THREE.GLTFLoader();
-loader.load('scene.gltf', function(gltf){
-  car = gltf.scene.children[0];
-  car.scale.set(0.5,0.5,0.5);
-  scene.add(gltf.scene);
-  animate();
-});
