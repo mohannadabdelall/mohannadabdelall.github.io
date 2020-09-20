88145f7af0098c496b1689a25f1a228db3582683
ahmadkammonah <57930224+ahmadkammonah@users.noreply.github.com>
2020-09-20 11:14:08 -0300

    Delete OrbitControls.js

--- OrbitControls.js
+++
@@ -1,7 +0,0 @@
-let controls = new THREE.OrbitControls(camera);
-controls.addEventListener('change', renderer);
-
-function animate() {
-  renderer.render(scene,camera);
-  requestAnimationFrame(animate);
-}
